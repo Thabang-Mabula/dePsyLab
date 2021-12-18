2a21efcb50ae181cffb9fb2c3ec2b969a44f6317
Thabang Mabula <1364103@students.wits.ac.za>
2021-12-18 19:50:38 +0200

    UI enhancements to options page

--- src/app/decision-matrix/pages/options-page/options-page.component.ts
+++ src/app/decision-matrix/pages/options-page/options-page.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DecisionMatrixRoutesEnum } from '../../constants/decision-matrix-routes-enum.enum';
+import { ParamEnum } from '../../constants/param-enum.enum';
+import { DecisionMatrix } from '../../entities/decision-matrix';
+import { DecisionMatrixAbstractService } from '../../services/decision-matrix-abstract-service';
 
 @Component({
   selector: 'app-options-page',
   templateUrl: './options-page.component.html',
-  styleUrls: ['./options-page.component.css']
+  styleUrls: ['./options-page.component.css'],
 })
 export class OptionsPageComponent implements OnInit {
+  private _decisionMatrix: DecisionMatrix = new DecisionMatrix();
+  private _title: string = '';
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private decisionMatrixService: DecisionMatrixAbstractService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {
+    this.activatedRoute.queryParams.subscribe((params) => {
+      let decisionMatrixId = params[ParamEnum.DECISION_MATRIX_ID];
+      this.decisionMatrixService
+        .getDecisionMatrix(decisionMatrixId)
+        .subscribe((decisionMatrix) => {
+          this._decisionMatrix = decisionMatrix;
+        });
+    });
   }
 
+  ngOnInit(): void {}
+
+  submit(): void {
+    this._decisionMatrix.title = this._title;
+    this.decisionMatrixService
+      .updateDecisionMatrix(this._decisionMatrix)
+      .subscribe((res) => {
+        this.navigateNext(this.generateNextURL(this._decisionMatrix.id!));
+      });
+  }
+
+  private navigateNext(url: string): void {
+    this.router.navigateByUrl(url);
+  }
+
+  private generateNextURL(id: string): string {
+    return `/${DecisionMatrixRoutesEnum.CONTEXT_ROOT}/${DecisionMatrixRoutesEnum.OPTIONS}?id=${id}`;
+  }
 }
