a1384343d047b06fe1326c822cfe8a8eb2543ff6
Thabang Mabula <1364103@students.wits.ac.za>
2021-12-18 17:03:07 +0200

    Moving pages into their own folder

--- src/app/decision-matrix/decision-matrix.module.ts
+++ src/app/decision-matrix/decision-matrix.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-import { DecisionMatrixStartComponent } from './components/decision-matrix-start/decision-matrix-start.component';
+import { DecisionMatrixStartComponent } from './pages/decision-matrix-start/decision-matrix-start.component';
 import { Routes, RouterModule } from '@angular/router';
 import { CommonComponentsModule } from '../common-components/common-components.module';
 import { CommonModule } from '@angular/common';
-import { DecisionMatrixComponent } from './components/decision-matrix/decision-matrix.component';
-import { NewTitlePageComponent } from './components/new-title-page/new-title-page.component';
+import { DecisionMatrixComponent } from './pages/decision-matrix/decision-matrix.component';
+import { NewTitlePageComponent } from './pages/new-title-page/new-title-page.component';
 
 const routes: Routes = [
   {
