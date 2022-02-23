43d2accb95eb8cf9087e026eca3c25ca45e7470c
Thabang Mabula <1364103@students.wits.ac.za>
2022-02-23 19:45:36 +0200

    Use the mock environment in PROD

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,13 @@
 import { DecisionMatrixAbstractService } from 'src/app/decision-matrix/services/decision-matrix-abstract-service';
-import { DecisionMatrixService } from '../app/decision-matrix/services/decision-matrix.service';
+import { MockDecisionMatrixService } from 'src/app/decision-matrix/services/mock-decision-matrix.service';
+
 export const environment = {
   production: true,
   providers: [
     {
       provide: DecisionMatrixAbstractService,
-      useClass: DecisionMatrixService,
+      // Use the mock decision matrix service since the real one has yet to be implemented
+      useClass: MockDecisionMatrixService,
     },
   ],
 };
