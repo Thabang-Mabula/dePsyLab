3fc3d19c746a5eab9a2a24aaeb34479bebfd0654
Thabang Mabula <1364103@students.wits.ac.za>
2022-02-23 19:50:25 +0200

    Removed unused imports from app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,9 @@
-import { DecisionMatrixService } from './decision-matrix/services/decision-matrix.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { CenterDivComponent } from './common-components/center-div/center-div.component';
-import { JourneyNavButtonComponent } from './common-components/journey-nav-button/journey-nav-button.component';
 import { DecisionMatrixModule } from './decision-matrix/decision-matrix.module';
-import { DecisionMatrixServiceInterface } from './decision-matrix/services/decision-matrix-service-interface';
-import { MockDecisionMatrixService } from './decision-matrix/services/mock-decision-matrix.service';
-import { DecisionMatrixAbstractService } from './decision-matrix/services/decision-matrix-abstract-service';
 import { environment } from 'src/environments/environment';
 import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
