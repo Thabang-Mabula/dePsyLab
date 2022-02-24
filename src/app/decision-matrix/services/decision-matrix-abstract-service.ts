6e54fd4694af5f4cf9892c2b4e03edbc0f04e6da
Thabang Mabula <1364103@students.wits.ac.za>
2022-02-24 23:14:04 +0200

    Cleanup on abtract and interface service classes

--- src/app/decision-matrix/services/decision-matrix-abstract-service.ts
+++ src/app/decision-matrix/services/decision-matrix-abstract-service.ts
@@ -3,27 +3,72 @@ import { Criterion } from '../entities/criterion';
 import { DecisionMatrix } from '../entities/decision-matrix';
 import { RankedOption } from '../entities/ranked-option';
 
+/**
+ * Abstract base class for the Decision Matrix Service
+ */
 export abstract class DecisionMatrixAbstractService {
   saveCriteria: Subject<any> = new Subject<any>();
 
+  /**
+   * Updates a given decision matrix
+   *
+   * @param  {DecisionMatrix} decisionMatrix Decision matrix
+   * @returns Observable that resolves a service response
+   */
   abstract updateDecisionMatrix(
     decisionMatrix: DecisionMatrix
   ): Observable<any>;
+
+  /**
+   * Get a decision matrix for a given id
+   *
+   * @param  {number} decisionMatrixId Decision matrix id
+   * @returns Observable that resolves a decision matrix
+   */
   abstract getDecisionMatrix(
     decisionMatrixId: number
   ): Observable<DecisionMatrix>;
+
+  /**
+   * Generate and retrieve a new decision matrix id
+   *
+   * @returns New decision matrix id
+   */
   abstract generateNewItemId(): string;
 
+  /**
+   * Retrieves the scored criteria for a given option
+   *
+   * @param  {string} decisionId Decision matrix id
+   * @param  {number} optionId Option id
+   * @returns Observable that resolves an array of criteria
+   */
   abstract retrieveCriteria(
     decisionId: string,
     optionId: number
   ): Observable<Array<Criterion>>;
 
+  /**
+   * Update the criteria
+   *
+   * @param  {string} decisionId Decision matrix id
+   * @param  {number} optionId Option id
+   * @param  {Array<Criterion>} criteria Criteria to update
+   * @returns Observable that resolves a service response
+   */
   abstract updateCriteria(
     decisionId: string,
     optionId: number,
     criteria: Array<Criterion>
   ): Observable<any>;
 
-  abstract getOptionRankings(decisionId: string): Observable<Array<RankedOption>>;
+  /**
+   * Get option rankings
+   *
+   * @param  {string} decisionId Decision matrix id
+   * @returns Observable array of ranked options
+   */
+  abstract getOptionRankings(
+    decisionId: string
+  ): Observable<Array<RankedOption>>;
 }
--- src/app/decision-matrix/services/decision-matrix-service-interface.ts
+++
@@ -1,6 +0,0 @@
-import { Observable } from "rxjs";
-
-export interface DecisionMatrixServiceInterface {
-  generateNewItemId(): string;
-  
-}
